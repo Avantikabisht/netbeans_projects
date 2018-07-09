c08716edf59dccf22acf0581e90cf06d046f3fc8
Avantikabisht <32274366+Avantikabisht@users.noreply.github.com>
2018-07-09 10:12:43 +0530

    Add files via upload

---
+++ BeabServlet1/src/java/Newpackage/Beanclass.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Newpackage;
+import java.io.Serializable;
+/**
+ *
+ * @author avantika
+ */
+public class Beanclass implements Serializable{
+  
+    private String name,age;
+    public Beanclass() {}
+     /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
